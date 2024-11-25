494e470d39e12a2790d106194f47d704ec59cf39
ryyexingchen <13061063929@163.com>
2024-11-26 01:22:47 +0800

    补充了lab3和练习2的代码

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -273,7 +273,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:EXERCISE2 22121518
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
